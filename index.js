6834082ef02ad37887006126c0fbeea3d9620fd0
my_laptop <dmitrij.kulik.d.v@gmail.com>
2025-06-02 19:37:40 +0300

    ewerithing is brocken

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const keyApi = "GviQzdKjoPGNyhx7DSnbn7zyBGqumbiShUSSVJEh"
 const getButton = document.querySelector(".get-photo-button")
-const photoField = document.querySelector(".photo-container")
+const photoField = document.querySelector(".photo-holder")
 const photoTitle = document.querySelector(".photo-title")
 const photoExplanation = document.querySelector(".photo-explanation")
 const author = document.querySelector(".author")
 const date = document.querySelector(".date")
+const cardTitle = document.querySelector(".card-title")
 
-getButton.addEventListener("click", getPhotoInformation)
+getButton.addEventListener("click", getPhotoOfTheDay)
 
 
-function getPhotoInformation() {
+function getPhotoOfTheDay() {
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${keyApi}`)
 .then((res) => {
     return res.json()
 })
 .then((res) => {
-    photoTitle.textContent = res.title
     photoExplanation.textContent = res.explanation
-    photoField.setAttribute("src", `${res.hdurl}`)
+    photoField.style.backgroundImage = `url('${res.hdurl}')`
     author.textContent = res.copyright
-    date.textContent = res.date
+    date.textContent = res.date.split("-").reverse().join(".")
+    cardTitle.textContent = res.title
 })
 }
