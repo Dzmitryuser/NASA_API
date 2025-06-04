0c4cffda0c3e98ec6181c61a8fd0ffaff5727edf
my_laptop <dmitrij.kulik.d.v@gmail.com>
2025-06-04 19:26:34 +0300

    almost working

--- index.js
+++ index.js
@@ -7,6 +7,54 @@ const cardList = document.querySelector(".card-list");
 
 getButton.addEventListener("click", getPhotoOfTheDay);
 
+//getPhotosByDate("2017-07-10", "2017-07-15");
+function getPhotosByDate(start, finish) {
+  fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${keyApi}&start_date=${start}&end_date=${finish}`
+  )
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res);
+      res.forEach((elem) => {
+        cardList.append(
+          cardRender(
+            elem.explanation,
+            elem.hdurl,
+            elem.copyright,
+            elem.date.split("-").reverse().join("."),
+            elem.title
+          )
+        );
+      });
+    })
+    .catch((error) => console.error("Ошибка при получении данных:", error));
+}
+
+//getSeveralPhotos(10)
+function getSeveralPhotos(num) {
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${keyApi}&count=${num}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      res.forEach((elem) => {
+        cardList.append(
+          cardRender(
+            elem.explanation,
+            elem.hdurl,
+            elem.copyright,
+            elem.date.split("-").reverse().join("."),
+            elem.title
+          )
+        );
+      });
+    })
+    .catch((error) => console.error("Ошибка при получении данных:", error));
+}
+
 function getPhotoOfTheDay() {
   fetch(`https://api.nasa.gov/planetary/apod?api_key=${keyApi}`)
     .then((res) => {
@@ -22,7 +70,8 @@ function getPhotoOfTheDay() {
           res.title
         )
       );
-    });
+    })
+    .catch((error) => console.error("Ошибка при получении данных:", error));
 }
 
 function cardRender(
